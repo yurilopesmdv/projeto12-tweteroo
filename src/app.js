f1015cf42d8b125e07004831131deed24e77daf2
yurilopesm <yurilopesm@hotmail.com>
2023-04-08 14:45:40 -0300

    requisito post tweets finalizado

--- src/app.js
+++ src/app.js
@@ -15,7 +15,16 @@ app.post("/sign-up", (req, res) => {
     res.send("OK")
 })
 
-
+app.post("/tweets", (req, res) => {
+    const {username, tweet} = req.body
+    const isAuthorized = users.find((user) => user.username === username)
+    if(!isAuthorized) {
+        return res.send("UNAUTHORIZED")
+    }
+    const newTweet = {username, tweet}
+    tweets.push(newTweet)
+    res.send("OK")
+})
 
 const PORT = 5000
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT} `))
