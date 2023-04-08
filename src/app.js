d7e99f882f82188a4271938c6629ffef0c628346
yurilopesm <yurilopesm@hotmail.com>
2023-04-08 14:39:47 -0300

    requisito post sign-up finalizado

--- src/app.js
+++ src/app.js
@@ -5,5 +5,17 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+const users = []
+const tweets = []
+
+app.post("/sign-up", (req, res) => {
+    const { username, avatar } = req.body
+    const user = { username, avatar}
+    users.push(user)
+    res.send("OK")
+})
+
+
+
 const PORT = 5000
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT} `))
